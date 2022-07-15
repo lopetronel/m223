a8c7225256e3d5c290ac842a8234d1e2da09eb4e
lopetronel <lopetronel@bzz.ch>
2022-07-15 12:45:31 +0200

    Delete

--- src/main/resources/META-INF/resources/script.js
+++ src/main/resources/META-INF/resources/script.js
@@ -1,30 +1,89 @@
 const URL = 'http://localhost:8080';
 let entries = [];
 
-const dateAndTimeToDate = (dateString, timeString) => {
-    return new Date(`${dateString}T${timeString}`).toISOString();
-};
+function openUpdateEntryForm() {
+    document.getElementById("updateEntryForm").style.display = "block";
+    document.getElementById("createEntryForm").style.display = "none";
+    document.getElementById("error").innerText = "";
+    document.getElementById("errorUpdate").innerText = "";
+}
+
+function closeUpdateEntryForm() {
+    document.getElementById("updateEntryForm").style.display = "none";
+    document.getElementById("createEntryForm").style.display = "block";
+    document.getElementById("error").innerText = "";
+    document.getElementById("errorUpdate").innerText = "";
+}
 
 const createEntry = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    const entry = {};
-    entry['checkIn'] = dateAndTimeToDate(formData.get('checkInDate'), formData.get('checkInTime'));
-    entry['checkOut'] = dateAndTimeToDate(formData.get('checkOutDate'), formData.get('checkOutTime'));
+    let data = {};
+    data["checkIn"] = formData.get("checkIn");
+    data["checkOut"] = formData.get("checkOut");
+            
 
     fetch(`${URL}/entries`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(entry)
+        body: JSON.stringify(data)
     }).then((result) => {
         result.json().then((entry) => {
-            entries.push(entry);
-            renderEntries();
+            if(entry.id == undefined) {
+                document.getElementById("error").innerText = entry.parameterViolations[0].message;
+            }else {
+                indexEntries();
+            }
+        });
+    }).catch((result) => {
+        result.json().then((response) => {
+            document.getElementById("error").innerText = response.parameterViolations[0].message;
         });
     });
 };
+
+const updateEntry = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    let data = {};
+    data["id"] = formData.get("id");
+    data["checkIn"] = formData.get("checkIn");
+    data["checkOut"] = formData.get("checkOut");
+
+    fetch(`${URL}/entries`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then((result) => {
+        result.json().then((entry) => {
+            if(entry.checkIn == undefined) {
+                document.getElementById("errorUpdate").innerText = entry.parameterViolations[0].message;
+            }else {
+                closeUpdateEntryForm();
+                indexEntries();
+            }
+        });
+    }).catch((result) => {
+        result.json().then((response) => {
+            document.getElementById("errorUpdate").innerText = response.parameterViolations[0].message;
+        });
+    });
+};
+
+
+const deleteEntry = (entry) => {
+    fetch(`${URL}/entries`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(entry)
+    });
+}
 
 const indexEntries = () => {
     fetch(`${URL}/entries`, {
@@ -52,6 +111,29 @@ const renderEntries = () => {
         row.appendChild(createCell(entry.id));
         row.appendChild(createCell(new Date(entry.checkIn).toLocaleString()));
         row.appendChild(createCell(new Date(entry.checkOut).toLocaleString()));
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = "Delete";
+        deleteButton.onclick = function() {
+            deleteEntry(entry);
+            indexEntries();
+        };
+        row.appendChild(deleteButton);
+
+        const updateButton = document.createElement('button');
+        updateButton.innerText = "Edit";
+        updateButton.onclick = function() {
+            let checkInDate = new Date(entry.checkIn);
+            checkInDate.setMinutes(checkInDate.getMinutes() - checkInDate.getTimezoneOffset());
+            let checkOutDate = new Date(entry.checkOut);
+            checkOutDate.setMinutes(checkOutDate.getMinutes() - checkOutDate.getTimezoneOffset());
+            document.getElementById("idUpdate").value = entry.id;
+            document.getElementById("checkInUpdate").value = checkInDate.toISOString().slice(0, 16);
+            document.getElementById("checkOutUpdate").value = checkOutDate.toISOString().slice(0, 16);
+            openUpdateEntryForm();
+        }
+        row.appendChild(updateButton);
+
         display.appendChild(row);
     });
 };
@@ -59,5 +141,7 @@ const renderEntries = () => {
 document.addEventListener('DOMContentLoaded', function(){
     const createEntryForm = document.querySelector('#createEntryForm');
     createEntryForm.addEventListener('submit', createEntry);
+    const updateEntryForm = document.querySelector('#updateEntryForm');
+    updateEntryForm.addEventListener('submit', updateEntry);
     indexEntries();
 });
